f3e18d77bad2bab48e1b2c040c3401c707102b63
mayankagarwal19911 <mayank.agarwal19911@gmail.com>
2024-01-26 15:33:15 +0000

    Add base code

---
+++ modules/ec2/main.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "instance"{
+    instance_type = var.instance_type
+    ami = var.ami
+
+    tags = {
+        Name = var.name   
+    }
+    vpc_security_group_ids = var.security_groups_id
+
+    user_data = <<-EOF
+        #!/bin/bash
+        yum update -y
+        yum install httpd -y
+        systemctl start httpd
+        systemctl enable httpd
+        echo "<html><body><div>Hello workd from $(hostname -f)</div></body></html>" > /var/www/html/index.html
+        EOF
+}
---
+++ modules/ec2/variables.tf
@@ -0,0 +1,15 @@
+variable "ami"{
+    type= string
+}
+
+variable "instance_type"{
+    type= string
+}
+
+variable "name"{
+    type= string
+}
+
+variable "security_groups_id"{
+    type = list
+}
